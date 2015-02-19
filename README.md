f72cd66588dcc2d1e9320bda1417635dce42db9a
Kent <register@gividen.com>
2015-02-19 13:03:27 -0700

    Added an interval

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 Photo frame widget for titanium alloy
 
 This was inspired in part by https://github.com/manumaticx/photogrid on how to display and flip through the photos.
+
+### Usage
+
+```javascript
+var photoWindow = Alloy.createWidget("AlloyPhotoFrameWidget").createWindow({
+    data: data,
+    interval: 3000
+});
+
+photoWindow.open();
+```
--- controllers/photoview.js
+++ controllers/photoview.js
@@ -1,5 +1,7 @@
 var args = arguments[0] || {},
     data = [];
+Ti.API.debug("args in photoview.js: " + JSON.stringify(args));
+Ti.API.debug("interval: " + args.interval);
 
 if (_.has(args, 'data')){
     _.each(args.data, function(photo){
@@ -16,6 +18,28 @@ if (_.has(args, 'data')){
     _.has(args, 'index') && $.photos.setCurrentPage(args.index);
 }
 
+function autoScroll() {
+    var intCurrentIndex = $.photos.getCurrentPage();
+    var intTotalViews = $.photos.getViews().length;
+    Ti.API.debug('loop: ' + intCurrentIndex);
+    if (intCurrentIndex === (intTotalViews - 1)) {
+        intCurrentIndex = 0;
+    } else {
+        intCurrentIndex = intCurrentIndex + 1;
+    }
+    $.photos.setCurrentPage(intCurrentIndex);
+}
+
+if (_.has(args, 'interval')) {
+    var intervalId = setInterval(function () {
+        autoScroll();
+    }, args.interval);
+}
+
+
 $.photos.addEventListener('click', function(){
     $.photos_win.close();
+    if (_.has(args, 'interval')) {
+        clearInterval(intervalId);
+    }
 });
